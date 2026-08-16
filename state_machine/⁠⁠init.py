685370c38b796bf50443105a853b34b4693f3e93
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-16 23:01:05 +0300

    Update ⁠⁠init.py

--- state_machine/⁠⁠init.py
+++ state_machine/⁠⁠init.py
@@ -1,1 +1,15 @@
-"""State machine layer."""
+"""State machine public API."""
+from state_machine.gates import GateBlockedError, GateResult, GateStatus, check_gate
+from state_machine.manager import GateBlockedError, InvalidTransitionError, StateManager
+from state_machine.transitions import ALLOWED_TRANSITIONS, is_transition_allowed
+
+__all__ = [
+    "ALLOWED_TRANSITIONS",
+    "GateBlockedError",
+    "GateResult",
+    "GateStatus",
+    "InvalidTransitionError",
+    "StateManager",
+    "check_gate",
+    "is_transition_allowed",
+]
