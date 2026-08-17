218acc11fbf44e23d2cf9dfe85869924861e4949
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-17 13:33:08 +0300

    Update __init__.py

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,9 +1,11 @@
 """Adapters public API."""
+from adapters.crossref import CrossrefAdapter
 from adapters.europe_pmc import EuropePMCAdapter
 from adapters.openalex import OpenAlexAdapter
 from adapters.pubmed import PubMedAdapter
 
 __all__ = [
+    "CrossrefAdapter",
     "EuropePMCAdapter",
     "OpenAlexAdapter",
     "PubMedAdapter",
