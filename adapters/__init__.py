ee5dae8705bfa8d12b612fe7960dc7f1440d4783
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-17 13:13:25 +0300

    Update __init__.py

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,6 +1,8 @@
 """Adapters public API."""
+from adapters.europe_pmc import EuropePMCAdapter
 from adapters.pubmed import PubMedAdapter
 
 __all__ = [
+    "EuropePMCAdapter",
     "PubMedAdapter",
 ]
