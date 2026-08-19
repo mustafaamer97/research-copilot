7c53fc0376fe66c6d8ef06a45a9aacc1ab077b34
mustafaamer97 <mostafa.abbas.amer20@gmail.com>
2026-08-19 23:51:54 +0300

    Create decision.py

---
+++ screening/decision.py
@@ -0,0 +1,7 @@
+"""Screening decision contract.
+Canonical ScreeningDecision is defined in domain.screening.
+This module provides the screening package import boundary.
+"""
+from domain.screening import ScreeningDecision
+
+__all__ = ["ScreeningDecision"]
